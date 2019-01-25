31847a841b6e1989b3672135721acbcc04107b4b
Tim Corr <tbcorr@gmail.com>
2019-01-24 22:40:11 -0500

    install vue-router, initial setup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+const routes = []
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+Vue.use(VueRouter);
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
