ccc8868f32179ee52b937bf412b3315b60e1ca8b
Tim Corr <tbcorr@gmail.com>
2019-01-24 23:19:16 -0500

    setup buefy, initial registration component setup

---
+++ src/components/Register.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="register">
+    <h1 class="title is-1">Tasks Vue</h1>
+    <p class="subtitle">Create a user and start adding tasks!</p>
+    <form class="registration-form">
+      <b-field label="Email" v-model="email">
+        <b-input type="email"></b-input>
+      </b-field>
+      <b-field label="Password">
+        <b-input type="text"></b-input>
+      </b-field>
+    </form>  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'register',
+
+  data () {
+    return {
+      email: "",
+      password: ""
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.register {
+  width: 33.33%;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+import '@/sass/styles.scss'
+
+import Register from './components/Register.vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
-const routes = []
+const routes = [
+  {
+    path: '/register',
+    component: Register
+  }
+]
 const router = new VueRouter({
   mode: 'history',
   routes
 })
 
-Vue.use(VueRouter);
+Vue.use(Buefy)
+Vue.use(VueRouter)
 
 new Vue({
   router,
