1840647d805b668db69b99a0df51dc0a5020d3e1
Tim Corr <tbcorr@gmail.com>
2019-02-20 20:38:27 -0500

    setup label input relationships

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -3,20 +3,20 @@
     <h1 class="title is-1">Tasks Vue</h1>
     <p class="subtitle">Create a user and start adding tasks!</p>
     <form class="registration-form" method="post">
-      <b-field label="First Name">
-        <b-input type="text" name="firstName" placeholder="first name" v-model="firstName"></b-input>
+      <b-field label="First Name" label-for="first-name">
+        <b-input id="first-name" type="text" name="firstName" placeholder="first name" v-model="firstName"></b-input>
       </b-field>
-      <b-field label="Last Name">
-        <b-input type="text" name="lastName" placeholder="last name" v-model="lastName"></b-input>
+      <b-field label="Last Name" label-for="last-name">
+        <b-input id="last-name" type="text" name="lastName" placeholder="last name" v-model="lastName"></b-input>
       </b-field>
-      <b-field label="Email">
-        <b-input type="email" name="email" placeholder="email" v-model="email"></b-input>
+      <b-field label="Email" label-for="email">
+        <b-input id="email" type="email" name="email" placeholder="email" v-model="email"></b-input>
       </b-field>
-      <b-field label="Password">
-        <b-input type="text" name="password" placeholder="password" v-model="email"></b-input>
+      <b-field label="Password" label-for="password">
+        <b-input id="password" type="text" name="password" placeholder="password" v-model="password"></b-input>
       </b-field>
-      <b-field label="Password (Confirm)">
-        <b-input type="text" name="passwordConfirm" placeholder="password (confirm)" v-model="email"></b-input>
+      <b-field label="Password (Confirm)" label-for="password-confirm">
+        <b-input id="password-confirm" type="text" name="passwordConfirm" placeholder="password (confirm)" v-model="passwordConfirm"></b-input>
       </b-field>
       <button class="button is-fullwidth">Submit</button>
     </form>  
@@ -29,8 +29,11 @@ export default {
 
   data () {
     return {
+      firstName: "",
+      lastName: "",
       email: "",
-      password: ""
+      password: "",
+      passwordConfirm: ""
     }
   }
 }
