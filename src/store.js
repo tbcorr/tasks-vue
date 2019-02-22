72faf0758393a99a07f47aa1afa324294b52d74d
Tim Corr <tbcorr@gmail.com>
2019-02-21 20:39:38 -0500

    add user & task getters, add task mutations

--- src/store.js
+++ src/store.js
@@ -10,10 +10,43 @@ const createStore = () => {
       },
       tasks: []
     },
-    mutations: {
-      increment (state) {
-        state.count++
+
+    getters: {
+      getTask: (state) => (id) => {
+        return state.tasks.find((task) => {
+          return task.id === id;
+        });
+      },
+
+      getTasks: (state) => {
+        return state.tasks;
+      },
+
+      getUser: (state) => {
+        return state.user;
       }
+    },
+
+    mutations: {
+      addTask: (state, task) => {
+        state.tasks = [...state.tasksList, ...task];
+      },
+
+      updateTask: (state, task) => {
+        const index = state.task.indexOf(task);
+
+        state.tasks = [...state.state.tasks.splice(index, 1), task];
+      },
+
+      removeTask: (state, task) => {
+        const index = state.task.indexOf(task);
+
+        state.tasks = [...state.state.tasks.splice(index, 1)];
+      }
+    },
+
+    actions: {
+
     }
   });
 };
